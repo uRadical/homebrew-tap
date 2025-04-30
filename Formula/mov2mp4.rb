91c25b2236e405206ad68949f4abcd526bc960c2
Alan <alan@uradical.io>
2025-04-30 14:32:05 +0100

    mov2mp4 v1.0.1

--- Formula/mov2mp4.rb
+++ Formula/mov2mp4.rb
@@ -1,8 +1,8 @@
 class Mov2mp4 < Formula
   desc "Command-line tool for converting MOV video files to MP4 format"
   homepage "https://github.com/uradical/mov2mp4"
-  url "https://github.com/uRadical/mov2mp4/releases/download/v1.0.0/mov2mp4-1.0.0.tar.gz"
-  sha256 "2e94f04fbfa511e8c1643bf568675876d07ca75eb1a0580e2f0fd5b277992fd8"
+  url "https://github.com/uRadical/mov2mp4/releases/download/v1.0.1/mov2mp4-1.0.1.tar.gz"
+  sha256 "8e0caf8ace3277d6ed7a2715de8f970717def1ce2d7f9a9498822a2f501c46d0"
   license "GPL-3.0"
   
   # Specify FFmpeg as a dependency
