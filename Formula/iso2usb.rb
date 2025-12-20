526393caaf7ce6b0656c8904540587c1bd84bfa6
Alan <alan@uradical.io>
2025-12-20 22:48:09 +0000

    update iso2usb url

--- Formula/iso2usb.rb
+++ Formula/iso2usb.rb
@@ -1,8 +1,9 @@
 class Iso2usb < Formula
   desc "TUI for writing ISO images to USB drives on macOS"
   homepage "https://github.com/uRadical/iso2usb"
-  url "https://raw.githubusercontent.com/uRadical/iso2usb/v1.0.0/iso2usb.sh"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/uRadical/iso2usb/releases/download/v1.0.0/iso2usb.sh",
+      using: :nounzip
+  sha256 "96acaf73c46c068acdee96563e105f322d2cd9f4753f7752d46d8fd73269f088"
   license "MIT"
 
   depends_on "dialog"
@@ -10,9 +11,5 @@ class Iso2usb < Formula
 
   def install
     bin.install "iso2usb.sh" => "iso2usb"
-  end
-
-  test do
-    assert_match "dialog is required", shell_output("#{bin}/iso2usb 2>&1", 1)
   end
 end
